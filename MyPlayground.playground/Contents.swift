c15a09ea219f70de7297ea79eaa8242c72f71f6d
Rami Sbahi <sbahirami@gmail.com>
2020-07-19 19:35:03 -0400

    last commit for stackmat for now

--- MyPlayground.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-import JavaScriptCore
-
-var str = "Hello, playground"
-
-let scramble_444_url = Bundle.main.url(forResource: "scramble_444", withExtension: "js")
-
-let jsContext: JSContext = JSContext.init()
-jsContext.evaluateScript("scramble_444.getRandomScramble()", withSourceURL: scramble_444_url)
